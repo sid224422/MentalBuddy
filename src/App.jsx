93874a73f2f7c42ced43502adfe77e2e9df3d75d
Siddhant Balkrishna Baviskar <s.sid202020@gmail.com>
2025-08-16 17:12:42 +0530

    Main_Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { LoadingSpinner } from './components/LoadingSpinner';
+
+// Lazy load page components
+const Home = lazy(() => import('./pages/Home').then(module => ({ default: module.Home })));
+const Chat = lazy(() => import('./pages/Chat').then(module => ({ default: module.Chat })));
+const Refresh = lazy(() => import('./pages/Refresh').then(module => ({ default: module.Refresh })));
+const Tools = lazy(() => import('./pages/Tools').then(module => ({ default: module.Tools })));
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/refresh" element={<Refresh />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/tools" element={<Tools />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
